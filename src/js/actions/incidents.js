8953f0dbd0f4b1d624968bf6686a47d929a0877e
Riley Durant <rileyduraant@gmail.com>
2019-01-12 22:34:06 -0500

    [FEAT]: Added refresh snackbar

--- src/js/actions/incidents.js
+++ src/js/actions/incidents.js
@@ -12,13 +12,39 @@ const incidentActions = {
     incidents,
   }),
 
-  fetchIncidents: () => async dispatch => {
+  fetchIncidents: () => async (dispatch, getStore) => {
     dispatch(incidentActions.requestIncidents());
 
     const incidents = await api.getAllIncidents();
 
     if (incidents.status === 200) {
+      // We'll need these to check if we're refreshing
+      // Thats why received is below
+      const numberOfcurrentIncidents = getStore().incidents.list.length;
+      const numberOfNewIncidents = incidents.values.length;
+
+      // Update the incidents in the store
       dispatch(incidentActions.receivedIncidents(incidents.values));
+
+      // If we have 0 current incidents, it's out first fetch, so we're not refreshing
+      if (numberOfcurrentIncidents !== 0) {
+        // Show the refresh snackbar
+        dispatch(
+          uiActions.openSnackbar({
+            message: `Refreshed`,
+          })
+        );
+        // If we have more new incidents than the current incidents, we got new incidents!
+        // Probably should do a better comparison since it's not 100% correct
+        if (numberOfNewIncidents > numberOfcurrentIncidents) {
+          dispatch(
+            uiActions.openSnackbar({
+              message: `${numberOfNewIncidents -
+                numberOfcurrentIncidents} New incidents `,
+            })
+          );
+        }
+      }
     } else {
       dispatch(uiActions.toggleModal(true, 'networkError'));
     }
