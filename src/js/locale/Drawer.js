293082c292b7f92e948f84299b4cb33f8f659b69
Riley Durant <rileyduraant@gmail.com>
2019-02-17 14:52:14 -0500

    [FIX]: Typo in search placeholder

--- src/js/locale/Drawer.js
+++ src/js/locale/Drawer.js
@@ -2,7 +2,7 @@ import { sorter } from '../helpers';
 
 const DrawerLocale = {
   header: {
-    searchPlaceholder: 'Search for Assualt, Dundas, etc...',
+    searchPlaceholder: 'Search for Assault, Dundas, etc...',
     sortItems: [
       { name: 'Newest', type: sorter.types.DATE_ASC, iconName: 'New' },
       { name: 'Oldest', type: sorter.types.DATE_DESC, iconName: 'Back' },
