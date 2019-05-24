7bf5bd51777925b1a8cfd6c26ecf4fe175a81532
Riley Durant <rileyduraant@gmail.com>
2019-05-23 21:18:21 -0400

    removed sentry

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -375,12 +375,14 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    /*
     new SentryCliPlugin({
       include: '/static/js',
       ignore: ['node_modules'],
       configFile: '.sentryclirc',
       release: packageJson.version.toString(),
     }),
+    */
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
