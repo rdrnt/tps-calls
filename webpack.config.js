0475e47259b641694b4b3a4e31aa179949cf56de
Riley Durant <rileyduraant@gmail.com>
2019-05-25 11:15:13 -0400

    [CHORE]: Updated config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const paths = {
   src: path.resolve('./src'),
 };
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = {
   entry: path.join(paths.src, '/index.tsx'),
@@ -12,27 +16,61 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
+        include: paths.src,
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
+      inject: true,
       template: path.join(paths.src, '/index.html'),
       filename: './index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
     }),
+    // new webpack.DefinePlugin(env.stringified),
   ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: isDevelopment
+      ? path.join('assets', 'js', '[name].js')
+      : path.join('assets', 'js', '[name].[hash:8].js'),
+    chunkFilename: isDevelopment
+      ? path.join('assets', 'js', '[name].chunk.js')
+      : path.join('assets', 'js', '[name].[hash:8].chunk.js'),
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true,
+        },
+        sourceMap: true,
+      }),
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
   },
   devServer: {
     host: 'localhost',
     port: 8080,
-    https: true,
+    // https: true,
     disableHostCheck: true,
     contentBase: paths.src,
     hot: true,
