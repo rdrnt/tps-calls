83f736a7c6618b325f704614b9121d27631d2203
Riley Durant <rileyduraant@gmail.com>
2019-09-19 11:27:45 -0400

    [FEAT]: Filtering incidents

--- src/components/IncidentListener/index.tsx
+++ src/components/IncidentListener/index.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Firebase } from '../../helpers';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
 import { setIncidentList } from '../../store/incidents/actions';
+import { AppState } from 'store';
+import { Incident } from 'tps-calls-shared';
+import { useDebouncedCallback } from 'use-debounce/lib';
 
-interface IncidentListenerProps {
-  dispatch: Dispatch;
-}
+const IncidentListener: React.FunctionComponent = ({}) => {
+  const dispatch = useDispatch();
+  const { filter, list } = useSelector((state: AppState) => state.incidents);
 
-const IncidentListener: React.FunctionComponent<IncidentListenerProps> = ({
-  dispatch,
-}) => {
+  // Store the default incidents
+  const [originalIncidentList, setOriginalIncidentList] = React.useState<
+    Incident<any>[]
+  >([]);
+
+  const [setIncidents] = useDebouncedCallback(incidents => {
+    dispatch(setIncidentList(incidents));
+  }, 300);
+
+  // Set the listener
   React.useEffect(() => {
     const incidentListener = Firebase.incidents.listener(newIncidents => {
-      dispatch(setIncidentList(newIncidents));
+      setIncidents(newIncidents);
+      setOriginalIncidentList(newIncidents);
     });
 
     return () => {
@@ -24,7 +35,37 @@ const IncidentListener: React.FunctionComponent<IncidentListenerProps> = ({
     };
   }, []);
 
+  // Filtering
+  React.useEffect(() => {
+    let filteredIncidents: Incident<any>[] = [];
+    if (filter.search) {
+      const matchingIncidents = list.filter(incident =>
+        incident.name.toLowerCase().includes(filter.search!.toLowerCase())
+      );
+      filteredIncidents.push(...matchingIncidents);
+    }
+
+    // if we have incidents to filter, remove the duplicates that may arise
+    if (filteredIncidents.length > 0) {
+      const filteredDuplicateIncidents = filteredIncidents.reduce(
+        (acc: Incident<any>[], current) => {
+          const x = acc.find(item => item.id === current.id);
+          if (!x) {
+            return acc.concat([current]);
+          } else {
+            return acc;
+          }
+        },
+        []
+      );
+
+      setIncidents(filteredDuplicateIncidents);
+    } else {
+      setIncidents(originalIncidentList);
+    }
+  }, [filter]);
+
   return null;
 };
 
-export default connect()(IncidentListener);
+export default IncidentListener;
