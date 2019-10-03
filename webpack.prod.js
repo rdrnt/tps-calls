e665ce11f116663855e7a1cc26acc8795118e97c
Riley Durant <rileyduraant@gmail.com>
2019-10-03 09:26:43 -0400

    [CHORE]: Removed sourcemaps

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,11 +4,8 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({
-      sourceMap: true,
-    })],
+    minimizer: [new TerserPlugin()],
   },
 });
