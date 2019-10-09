2e8c4578785e51e24aff840065e1a05e28216a32
Riley Durant <rileyduraant@gmail.com>
2019-10-09 13:47:11 -0400

    [FEAT]: Calculate mix/max time

--- src/components/Drawer/List/Filter/Date.tsx
+++ src/components/Drawer/List/Filter/Date.tsx
@@ -9,6 +9,9 @@ import Icon from '../../../Icon';
 import { Button } from '../../../Button';
 import { DateHelper } from '../../../../helpers';
 import { onHover } from '../../../../helpers/hooks';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../../../store';
+import { setHours, setMinutes } from 'date-fns';
 
 const ItemContainer = styled.button<{ color: string }>`
   margin: 0;
@@ -60,8 +63,10 @@ const DateFilterItem: React.FunctionComponent<DateFilterItem> = ({
 
 interface CalendarConfig {
   id: 'start' | 'end';
-  value?: Timestamp;
+  value: Timestamp;
   onChange: (newDate: Date) => void;
+  min?: DateHelper.Timestamp;
+  max?: DateHelper.Timestamp;
 }
 
 interface DateFilter {
@@ -112,6 +117,35 @@ const Content = styled.div`
   align-items: center;
 `;
 
+const calculateMinTime = (currentValue: Date, oldestValue: Timestamp) => {
+  const oldestValueDate = DateHelper.convertTimestampToDate(oldestValue);
+
+  if (currentValue.getDate() === oldestValueDate.getDate()) {
+    console.log('Matching min time', oldestValueDate.toLocaleTimeString());
+    return oldestValueDate;
+  }
+
+  console.log('Not Matching min time', currentValue.toLocaleTimeString());
+  return setHours(setMinutes(new Date(), 0), 0);
+};
+
+const calculateMaxTime = (currentValue: Date, newestValue: Timestamp) => {
+  const newestValueDate = DateHelper.convertTimestampToDate(newestValue);
+
+  if (currentValue.getDate() === newestValueDate.getDate()) {
+    console.log('Matching Max time', newestValueDate.toLocaleTimeString());
+    return newestValueDate;
+  }
+  console.log('Not mathcing max time', currentValue.toLocaleTimeString());
+  return setHours(
+    setMinutes(new Date(), currentValue.getHours()),
+    currentValue.getMinutes()
+  );
+};
+
+// Requirements
+// The start date cannot be older than the oldest incident
+// The end date cannot be newer than the newest incident
 const DateFilter: React.FunctionComponent<DateFilter> = ({
   startDate,
   endDate,
@@ -119,6 +153,11 @@ const DateFilter: React.FunctionComponent<DateFilter> = ({
   setStartDate,
 }) => {
   const [calendar, showCalendar] = React.useState<CalendarConfig | undefined>();
+  const [errorMessage, setErrorMessage] = React.useState<string>('');
+  const incidentDates = useSelector((state: AppState) => ({
+    oldest: state.incidents.oldestIncidentDate,
+    newest: state.incidents.newestIncidentDate,
+  }));
 
   const hideCalendar = () => {
     showCalendar(undefined);
@@ -130,6 +169,63 @@ const DateFilter: React.FunctionComponent<DateFilter> = ({
       hideCalendar();
     } else {
       showCalendar(config);
+    }
+  };
+
+  const changeStartDate = (newStartDate: Date) => {
+    const oldestIncidentDate = DateHelper.convertTimestampToDate(
+      incidentDates.oldest
+    );
+    console.log(
+      'Setting new start date to',
+      DateHelper.formatIncidentDate(
+        DateHelper.convertDateToTimestamp(newStartDate)
+      )
+    );
+    if (newStartDate < oldestIncidentDate) {
+      setErrorMessage(
+        'Start date cannot be older than ' +
+          DateHelper.formatIncidentDate(incidentDates.oldest)
+      );
+    } else {
+      setStartDate(DateHelper.convertDateToTimestamp(newStartDate));
+    }
+  };
+
+  const calculateMinMaxTime = (
+    currentValue: DateHelper.Timestamp,
+    newestValue: DateHelper.Timestamp,
+    oldestValue: DateHelper.Timestamp
+  ) => {
+    const currentValueDate = DateHelper.convertTimestampToDate(currentValue);
+    const newestValueDate = DateHelper.convertTimestampToDate(newestValue);
+    const oldestValueDate = DateHelper.convertTimestampToDate(oldestValue);
+
+    // if the current value is equal to the newest incident
+    if (
+      currentValueDate.getDate() === newestValueDate.getDate() &&
+      currentValueDate.getDate() !== oldestValueDate.getDate()
+    ) {
+      return {
+        minTime: setHours(setMinutes(new Date(), 0), 0),
+        maxTime: setHours(
+          setMinutes(new Date(), newestValueDate.getMinutes()),
+          newestValueDate.getHours()
+        ),
+      };
+    }
+
+    if (
+      currentValueDate.getDate() === oldestValueDate.getDate() &&
+      currentValueDate.getDate() !== newestValueDate.getDate()
+    ) {
+      return {
+        minTime: setHours(
+          setMinutes(new Date(), oldestValueDate.getMinutes()),
+          oldestValueDate.getHours()
+        ),
+        maxTime: setHours(setMinutes(new Date(), 59), 23),
+      };
     }
   };
 
@@ -156,9 +252,10 @@ const DateFilter: React.FunctionComponent<DateFilter> = ({
             onClick={() =>
               showCalendarWithConfig({
                 id: 'start',
-                value: startDate,
-                onChange: (value: Date) =>
-                  setStartDate(DateHelper.convertDateToTimestamp(value)),
+                value: startDate || DateHelper.now(),
+                onChange: changeStartDate,
+                min: incidentDates.oldest,
+                max: incidentDates.newest,
               })
             }
             active={Boolean(calendar && calendar.id === 'start')}
@@ -171,9 +268,11 @@ const DateFilter: React.FunctionComponent<DateFilter> = ({
                 onClick={() =>
                   showCalendarWithConfig({
                     id: 'end',
-                    value: endDate,
+                    value: endDate || DateHelper.now(),
                     onChange: (value: Date) =>
                       setEndDate(DateHelper.convertDateToTimestamp(value)),
+                    min: startDate,
+                    max: incidentDates.newest,
                   })
                 }
                 active={Boolean(calendar && calendar.id === 'end')}
@@ -183,17 +282,24 @@ const DateFilter: React.FunctionComponent<DateFilter> = ({
         </DateFilterContent>
         {calendar && (
           <CalendarContent>
+            {errorMessage && <Text as="span">{errorMessage}</Text>}
             <DatePicker
-              selected={
-                calendar.value
-                  ? DateHelper.convertTimestampToDate(
-                      calendar.value as Timestamp
-                    )
-                  : new Date()
-              }
+              selected={DateHelper.convertTimestampToDate(calendar.value)}
               onChange={calendar.onChange}
               inline={true}
               showTimeSelect={true}
+              minDate={
+                calendar.min && DateHelper.convertTimestampToDate(calendar.min)
+              }
+              maxDate={
+                calendar.max && DateHelper.convertTimestampToDate(calendar.max)
+              }
+              {...calculateMinMaxTime(
+                calendar.value,
+                incidentDates.newest,
+                incidentDates.oldest
+              )}
+              timeIntervals={15}
               calendarClassName="customCalendar"
             />
             <Button label="close" onClick={hideCalendar} />
