440c3b1c3ac4026c5606d5ff3dce1bd9150243bd
riley drnt <rileyduraant@gmail.com>
2025-10-25 22:02:21 -0400

    [STYLE]: Re-did contact page

--- src/containers/Download.tsx
+++ src/containers/Download.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import styled from 'styled-components';
+import { Map } from 'lucide-react';
 
 import { Typography } from '../components/Typography';
 import { Button } from '../components/ui/button';
-import { Colors, Sizes } from '../config';
-import { Analytics } from '../helpers';
+import { Container } from '../components/ui/container';
+import { Separator } from '../components/ui/separator';
 
 import AppStoreDownloadImage from '../assets/images/appStoreDownload.svg';
 import PlayStoreDownloadImage from '../assets/images/googlePlayDownload.svg';
 import AppLogo from '../assets/images/appStoreIcon.jpg';
+import useAnalyticsPageView from '../hooks/useAnalyticsPageView';
 
 const APPSTORE_DOWNLOAD_LINK =
   'https://apps.apple.com/us/app/tpscalls/id6502376708';
@@ -16,180 +16,58 @@ const APPSTORE_DOWNLOAD_LINK =
 const PLAYSTORE_DOWNLOAD_LINK =
   'https://play.google.com/store/apps/details?id=com.drnt.tpscalls';
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-color: ${Colors.BACKGROUND};
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  > button {
-    margin-top: ${Sizes.SPACING}px;
-    color: ${Colors.BACKGROUND};
-  }
-`;
-
-const StyledHeadingContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-
-  img {
-    height: 250px;
-    width: 250px;
-    border-radius: 25px;
-  }
-
-  > h2 {
-  }
-
-  > h6 {
-  }
-
-  > p {
-    color: ${Colors.TEXT_SECONDARY};
-    font-weight: 300;
-    letter-spacing: 1px;
-    padding: ${Sizes.SPACING}px;
-    max-width: 60%;
-
-    @media only screen and (max-width: 600px) {
-      max-width: 100%;
-      padding: ${Sizes.SPACING}px;
-    }
-  }
-`;
-
-const StyledDownloadButtonContent = styled.div`
-  display: flex;
-  flex-dirction: row;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  padding: ${Sizes.SPACING}px;
-  padding-top: 0;
-
-  a {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  img {
-    width: 200px;
-  }
-
-  .spacer {
-    background-color: transparent;
-    height: auto;
-    width: ${Sizes.SPACING * 4}px;
-  }
-
-  @media only screen and (max-width: 600px) {
-    img {
-      width: 175px;
-    }
-
-    .spacer {
-      background-color: transparent;
-      height: ${Sizes.SPACING}px;
-      width: ${Sizes.SPACING}px;
-    }
-  }
-`;
-
-const StyledDividerContent = styled.div`
-  width: 100%;
-  max-width: 700px;
-  background-color: transparent;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-  padding: 0 ${Sizes.SPACING * 2}px;
-
-  .divider {
-    height: 2px;
-    width: 100%;
-    background-color: ${Colors.BORDER};
-  }
-
-  > p {
-    margin: 0 20px;
-    color: ${Colors.BORDER};
-  }
-`;
-
-const StyledBottomDownloadContent = styled.div`
-  padding: ${Sizes.SPACING}px;
-
-  > a {
-    > button {
-      padding: ${Sizes.SPACING}px;
-      color: ${Colors.BACKGROUND};
-      font-weight: 500;
-
-      font-size: 12px;
-      letter-spacing: 1px;
-      text-decoration: none;
-    }
-  }
-`;
-
 const DownloadPage = () => {
-  React.useEffect(() => {
-    Analytics.pageview('/download');
-  }, []);
+  useAnalyticsPageView({ path: '/download' });
 
   return (
-    <Container>
-      <StyledHeadingContent>
-        <img src={AppLogo} />
-        <Typography variant="h2">tpscalls</Typography>
-        <Typography variant="h6">By Riley Durant</Typography>
+    <div className="bg-zinc-100 h-full w-full">
+      <Container>
+        <div className="flex flex-col items-start justify-center h-full min-h-svh">
+          <img
+            src={AppLogo}
+            alt="App Logo"
+            className="w-18 h-18 rounded-md shadow-md mb-4"
+          />
+          <Typography variant="h2" className="mb-2">
+            Mobile App
+          </Typography>
 
-        <Typography variant="p">
-          tpscalls is now available on mobile devices, bringing you a real-time
-          map of Toronto Police response locations. Track incidents like
-          arrests, gun calls, and collisions right from your phone. Stay
-          informed with instant updates, wherever you are.
-        </Typography>
-      </StyledHeadingContent>
+          <Typography variant="p">
+            tpscalls is now available on mobile devices, bringing you a
+            real-time map of Toronto Police response locations. Track incidents
+            like arrests, gun calls, and collisions right from your phone. Stay
+            informed with instant updates, wherever you are.
+          </Typography>
 
-      <StyledDownloadButtonContent>
-        <a
-          href={APPSTORE_DOWNLOAD_LINK}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={AppStoreDownloadImage} />
-        </a>
-        <div className="spacer" />
-        <a
-          href={PLAYSTORE_DOWNLOAD_LINK}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={PlayStoreDownloadImage} />
-        </a>
-      </StyledDownloadButtonContent>
-      <StyledDividerContent>
-        <div className="divider" />
-        <Typography variant="p">OR</Typography>
-        <div className="divider" />
-      </StyledDividerContent>
-      <StyledBottomDownloadContent>
-        <Button asChild>
-          <a href="/">
-            <Typography variant="p">VIEW ON THE WEB</Typography>
-          </a>
-        </Button>
-      </StyledBottomDownloadContent>
-    </Container>
+          <div className="flex flex-col items-start justify-center mt-6 ">
+            <div className="flex flex-row items-center justify-center gap-4">
+              <a
+                href={APPSTORE_DOWNLOAD_LINK}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={AppStoreDownloadImage} />
+              </a>
+              <a
+                href={PLAYSTORE_DOWNLOAD_LINK}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={PlayStoreDownloadImage} />
+              </a>
+            </div>
+            <Separator className="my-4 w-full" />
+
+            <Button asChild>
+              <a href="/">
+                <Map />
+                <Typography variant="p">View On The Web</Typography>
+              </a>
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 };
 
