a1695812b5c655910f4062baf5a8cb7c6613db6c
riley drnt <rileyduraant@gmail.com>
2025-11-03 22:34:41 -0500

    [CHORE]: Cameras auto update

--- src/components/Listeners/Cameras.tsx
+++ src/components/Listeners/Cameras.tsx
@@ -1,21 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { toast } from 'sonner';
 import { firestore } from '../../helpers/firebase';
 import { TorontoTrafficCamera } from '../../containers/BetaFeature';
 import { collection, getDocs, orderBy, query } from 'firebase/firestore';
 import { useAppDispatch } from '../../store';
 import { setCameraList } from '../../store/actions';
 
+const CAMERAS_REFRESH_INTERVAL_IN_MINUTES = 2.5;
+
 const TorontoCamerasListener = () => {
   const dispatch = useAppDispatch();
-  useEffect(() => {
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const fetchCameras = async () => {
     const camerasCollection = collection(firestore, 'toronto-traffic-cameras');
     const camerasQuery = query(camerasCollection, orderBy('date', 'desc'));
-    getDocs(camerasQuery).then(docs => {
-      const cameras = docs.docs.map(doc => doc.data() as TorontoTrafficCamera);
+    const docs = await getDocs(camerasQuery);
+    const cameras = docs.docs.map(doc => doc.data() as TorontoTrafficCamera);
+    dispatch(setCameraList(cameras));
+  };
 
-      dispatch(setCameraList(cameras));
-    });
+  useEffect(() => {
+    fetchCameras();
+
+    timer.current = setTimeout(
+      () => {
+        fetchCameras();
+        toast.success('Cameras refreshed');
+      },
+      CAMERAS_REFRESH_INTERVAL_IN_MINUTES * 60 * 1000
+    );
+
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+    };
   }, []);
+
   return null;
 };
 
